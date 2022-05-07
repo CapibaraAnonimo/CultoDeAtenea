b42866a95d1e80d1deb26b0d5f7fba3046617bb9
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-05-07 20:36:03 +0200

    Más o menos hecho el prototipo para la sección de noticias

--- CultoDeAtenea/src/main/java/com/salesianostriana/dam/cultodeatenea/ControllerGeneral.java
+++ CultoDeAtenea/src/main/java/com/salesianostriana/dam/cultodeatenea/ControllerGeneral.java
@@ -1,7 +1,10 @@
 package com.salesianostriana.dam.cultodeatenea;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class ControllerGeneral {
 	@GetMapping("admin/noticias")
 	public String noticias() {
