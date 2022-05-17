c235d5f3227a0a656757b4ff99511e1506271744
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-05-17 14:19:25 +0200

    He avanzado en el carrito, yo creo que esta tarde lo termino, a ver. Me veo optimista con el proyecto de repente, pero no es que vaya sobrado de tiempo

---
+++ CultoDeAtenea/src/main/java/com/salesianostriana/dam/cultodeatenea/controller/CarritoController.java
@@ -0,0 +1,40 @@
+package com.salesianostriana.dam.cultodeatenea.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.salesianostriana.dam.cultodeatenea.model.Producto;
+import com.salesianostriana.dam.cultodeatenea.service.CarritoService;
+import com.salesianostriana.dam.cultodeatenea.service.ProductoService;
+
+@Controller
+public class CarritoController {
+
+	@Autowired
+	private CarritoService carritoService;
+
+	@Autowired
+	private ProductoService productoService;
+
+	@GetMapping("/carrito")
+	public String mostrarCarrito(Model model) {
+		if (model.addAttribute("products", carritoService.getProductsInCart()) == null)
+			return "redirect:/";
+		return "carrito";
+	}
+	
+	@GetMapping ("/productoCarrito/{id}")
+    public String productoACarrito (@PathVariable("id") Long id, Model model) {
+    	Optional<Producto> p = productoService.findById(id);
+    	if(p.isPresent()) {
+    		carritoService.addProducto(p.get());
+    	}
+    	return "redirect:/carrito";
+    }
+
+}
---
+++ CultoDeAtenea/src/main/java/com/salesianostriana/dam/cultodeatenea/service/CarritoService.java
@@ -0,0 +1,51 @@
+package com.salesianostriana.dam.cultodeatenea.service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.salesianostriana.dam.cultodeatenea.model.Producto;
+import com.salesianostriana.dam.cultodeatenea.repository.ProductoRepository;
+
+@Service
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class CarritoService {
+
+	private ProductoRepository productoRepository;
+
+	private Map<Producto, Integer> products = new HashMap<>();
+
+	@Autowired
+	public CarritoService(ProductoRepository productoRepository) {
+		this.productoRepository = productoRepository;
+	}
+
+	public void addProducto(Producto p) {
+		if (products.containsKey(p)) {
+			products.replace(p, products.get(p) + 1);
+		} else {
+			products.put(p, 1);
+		}
+	}
+
+	public void removeProducto(Producto p) {
+		if (products.containsKey(p)) {
+			if (products.get(p) > 1)
+				products.replace(p, products.get(p) - 1);
+			else if (products.get(p) == 1) {
+				products.remove(p);
+			}
+		}
+	}
+
+	public Map<Producto, Integer> getProductsInCart() {
+		return Collections.unmodifiableMap(products);
+	}
+
+}
